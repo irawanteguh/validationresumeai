e2fbe1a0905eba1d3b37b0539395f3042739cf87
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2026-06-08 13:51:58 +0700

    update

--- db.py
+++ db.py
@@ -2,54 +2,48 @@ import oracledb
 from config import DB_CONFIG
 
 # =========================================
-# ORACLE MODE (THIN MODE - LINUX SAFE)
+# INIT ORACLE CLIENT (WINDOWS)
 # =========================================
-# TIDAK PERLU init_oracle_client di Linux
 
-oracledb.defaults.fetch_lobs = False  # optional optimize
-
+oracledb.init_oracle_client(
+    lib_dir=DB_CONFIG["oracleclient"]
+)
 
 # =========================================
 # CONNECTION
 # =========================================
 def get_connection():
-    try:
-        dsn = oracledb.makedsn(
-            DB_CONFIG["host"],
-            DB_CONFIG["port"],
-            service_name=DB_CONFIG["service_name"]
-        )
 
-        conn = oracledb.connect(
-            user=DB_CONFIG["user"],
-            password=DB_CONFIG["password"],
-            dsn=dsn
-        )
+    dsn = oracledb.makedsn(
+        DB_CONFIG["host"],
+        DB_CONFIG["port"],
+        service_name = DB_CONFIG["service_name"]
+    )
 
-        return conn
+    conn = oracledb.connect(
+        user     = DB_CONFIG["user"],
+        password = DB_CONFIG["password"],
+        dsn      = dsn
+    )
 
-    except Exception as e:
-        print(f"[DB ERROR] {e}")
-        raise
+    return conn
 
 
 # =========================================
-# FETCH DATA (SAFE VERSION)
+# FETCH DATA
 # =========================================
 def fetch_data():
+    
 
-    conn = None
-    cursor = None
+    conn = get_connection()
+    cursor = conn.cursor()
 
-    try:
-        conn = get_connection()
-        cursor = conn.cursor()
-
-        query = """
+    query = """
         SELECT 
             d.episode_id,
             d.dokter_id,
-            (SELECT nama FROM sr01_med_dokter_ms WHERE dokter_id=d.dokter_id) namadokter,
+
+            (SELECT nama FROM sr01_med_dokter_ms WHERE dokter_id=d.dokter_id)namadokter,
 
             d.keluhan_utama          AS dokter_keluhan,
             d.gejala_penyerta        AS dokter_gejala,
@@ -77,7 +71,15 @@ def fetch_data():
             SELECT *
             FROM web_co_resume_ranap_ai
             WHERE show_item ='1'
-            OR (show_item = 'Y' AND kontrol LIKE 'Kontrol ulang%')
+            OR (show_item = 'Y' AND kontrol = 'Kontrol ulang ke POLI PENYAKIT DALAM')
+            OR (show_item = 'Y' AND kontrol = 'Kontrol ulang ke POLI PARU')
+            OR (show_item = 'Y' AND kontrol = 'Kontrol ulang ke POLI ANAK')
+            OR (show_item = 'Y' AND kontrol = 'Kontrol ulang ke POLI JANTUNG')
+            OR (show_item = 'Y' AND kontrol = 'Kontrol ulang ke POLI SARAF')
+            OR (show_item = 'Y' AND kontrol = 'Kontrol ulang ke POLI KEBIDANAN')
+            OR (show_item = 'Y' AND kontrol = 'Kontrol ulang ke POLI ORTHOPEDI')
+            OR (show_item = 'Y' AND kontrol = 'Kontrol ulang ke POLI BEDAH UMUM')
+            OR (show_item = 'Y' AND kontrol = 'Kontrol ulang ke POLI UROLOGI')
         ) ai
 
         JOIN (
@@ -87,36 +89,65 @@ def fetch_data():
                     s.*,
                     ROW_NUMBER() OVER (
                         PARTITION BY s.episode_id
-                        ORDER BY s.created_date DESC
+                        ORDER BY s.created_date DESC, s.rowid DESC
                     ) rn
                 FROM sr01_resume_medis s
                 WHERE s.aktif in ('1','2','9')
-                  AND s.poli_id is null
-                  AND s.created_by like 'DR%'
+                and   s.poli_id is null
+                and   s.created_by like 'DR%'
             )
             WHERE rn = 1
         ) d
         ON d.episode_id = ai.episode_id
+        where EXISTS (
+                        SELECT 1
+                        FROM sr01_keu_episode e
+                        WHERE e.lokasi_id = '001'
+                        AND e.aktif = '1'
+                        AND e.jenis_episode = 'I'
+                        AND e.status_episode = '55'
+                        AND e.pasien_id = ai.pasien_id
+                        AND e.episode_id = ai.episode_id
+                    )
+        AND NOT EXISTS (
+        				SELECT 1
+        				FROM web_co_registrasi_online_hd
+        				WHERE lokasi_id='001'
+        				AND show_item='1'
+        				AND pasien_id=ai.pasien_id
+        				AND episode_id=ai.episode_id
+        			)
+        -- AND ai.created_date <= TO_DATE('26-05-2026 23:59:59','DD-MM-YYYY HH24:MI:SS')
+        -- AND ai.created_date >= TO_DATE('01-06-2026 23:59:59','DD-MM-YYYY HH24:MI:SS')
+        -- AND ai.episode_id='126053153330'
+        -- AND d.dokter_id='DR. H0000000005'
+    """
 
-        """
+    cursor.execute(query)
 
-        cursor.execute(query)
+    # =========================================
+    # AMBIL NAMA KOLOM OTOMATIS
+    # =========================================
+    columns = [col[0].lower() for col in cursor.description]
 
-        columns = [col[0].lower() for col in cursor.description]
+    data = []
 
-        data = [
-            dict(zip(columns, row))
-            for row in cursor
-        ]
+    # =========================================
+    # DYNAMIC MAPPING
+    # =========================================
+    for row in cursor:
 
-        return data
+        item = {
+            column: value
+            for column, value in zip(columns, row)
+        }
 
-    except Exception as e:
-        print(f"[FETCH ERROR] {e}")
-        return []
+        data.append(item)
 
-    finally:
-        if cursor:
-            cursor.close()
-        if conn:
-            conn.close()
+    # =========================================
+    # CLOSE
+    # =========================================
+    cursor.close()
+    conn.close()
+
+    return data
