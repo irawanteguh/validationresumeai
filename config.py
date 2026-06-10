365b2ca23bcb9cd5ec58eb6f5a8c105826e5f5a5
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2026-06-10 08:22:29 +0700

    exclude config and monitoring state

--- config.py
+++
@@ -1,9 +0,0 @@
-DB_CONFIG = {
-    "host"        : "rac-scan.rsudpm.local",
-    "port"        : 1521,
-    "service_name": "PRODPM",
-    "user"        : "SIMRS_MANAGER",
-    "password"    : "SIMRS_SYSTEM",
-    "oracleclient": r"E:\xampp\instantclient_11_2"
-    # "oracleclient": "/Users/teguhirawan/oracle/instantclient_19_16"
-}
